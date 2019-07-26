450c9e825b49132b8bb313d1f041441d3e9ea6e7
王灏轩 <https://gitee.com/whxwhx123/yunmusic.git>
2019-07-26 19:53:02 +0800

    第一次提交作业
    提交作业

--- src/main/java/com/Haoxuan/wacai/web/Application.java
+++ src/main/java/com/Haoxuan/wacai/web/Application.java
@@ -2,7 +2,7 @@ package com.Haoxuan.wacai.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//spring
 @SpringBootApplication
 public class Application {
 
